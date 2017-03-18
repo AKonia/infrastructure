3ed7f874f9a08600a2bd1046ef33688487267982
Александр <konakov@my.com>
2017-03-18 22:26:15 +0300

    Переделано под cpplint

--- include/bin_tree.h
+++ include/bin_tree.h
@@ -2,22 +2,23 @@
 #define INCLUDE_BIN_TREE_H_
 
 struct bin_node {
-    int value;
-    bin_node * left, * right;
+  int value;
+  bin_node *left, *right;
 };
 
 class bin_tree {
  public:
-        bin_tree();
-        ~bin_tree();
-        int search(int value);
-        bin_node ** search_for_insert(int value);
-        int insert(int value);
-        int get_size();
-        bin_node * bin_bal_tree_creat(int * a, int size);
+  bin_tree();
+  ~bin_tree();
+  int search(int value);
+  bin_node **search_for_insert(int value);
+  int insert(int value);
+  int get_size();
+  bin_node *bin_bal_tree_creat(int *a, int size);
+
  private:
-        int tree_size;
-        bin_node *root;
+  int tree_size;
+  bin_node *root;
 };
 
 #endif  // INCLUDE_BIN_TREE_H_
--- test/test_bal_tree.cpp
+++ test/test_bal_tree.cpp
@@ -1,34 +1,32 @@
-#include "bin_tree.h"
 #include <gtest/gtest.h>
-
+#include "../include/bin_tree.h"
 
 TEST(BalancedTree, can_create_tree) {
-    const int arr_size = 5;    
-    int a[] = {1, 2, 6, 7, 9};
-    bin_tree tree;
-    ASSERT_NO_THROW(tree.bin_bal_tree_creat(a, arr_size));
+  const int arr_size = 5;
+  int a[] = {1, 2, 6, 7, 9};
+  bin_tree tree;
+  ASSERT_NO_THROW(tree.bin_bal_tree_creat(a, arr_size));
 }
 
 TEST(BalancedTree, all_inserted_elements_exists) {
-    const int arr_size = 7;    
-    int a[] = {1, 2, 6, 7, 9, 10, 11};
-    bin_tree tree;
-    ASSERT_NO_THROW(tree.bin_bal_tree_creat(a, arr_size));
-    EXPECT_EQ(arr_size, tree.get_size());
-    for(int i = 0; i < arr_size; i++)
-        EXPECT_EQ(tree.search(a[i]), 1);
+  const int arr_size = 7;
+  int a[] = {1, 2, 6, 7, 9, 10, 11};
+  bin_tree tree;
+  ASSERT_NO_THROW(tree.bin_bal_tree_creat(a, arr_size));
+  EXPECT_EQ(arr_size, tree.get_size());
+  for (int i = 0; i < arr_size; i++) EXPECT_EQ(tree.search(a[i]), 1);
 }
 
 TEST(BalancedTree, tree_is_really_balanced) {
-    const int arr_size = 5;    
-    int a[] = {1, 2, 6, 7, 9};
-    bin_tree tree;
-    bin_node * root;
-    ASSERT_NO_THROW(root = tree.bin_bal_tree_creat(a, arr_size));
-    EXPECT_EQ(arr_size, tree.get_size());
-    EXPECT_EQ(root->value, 6);
-    EXPECT_EQ(root->left->value, 1);
-    EXPECT_EQ(root->right->value, 7);
-    EXPECT_EQ(root->left->right->value, 2);
-    EXPECT_EQ(root->right->right->value , 9);
+  const int arr_size = 5;
+  int a[] = {1, 2, 6, 7, 9};
+  bin_tree tree;
+  bin_node* root;
+  ASSERT_NO_THROW(root = tree.bin_bal_tree_creat(a, arr_size));
+  EXPECT_EQ(arr_size, tree.get_size());
+  EXPECT_EQ(root->value, 6);
+  EXPECT_EQ(root->left->value, 1);
+  EXPECT_EQ(root->right->value, 7);
+  EXPECT_EQ(root->left->right->value, 2);
+  EXPECT_EQ(root->right->right->value, 9);
 }
