27cef9ca0ffe16301bd802ff2191e3895aa4e583
Alexander <konakov.a@ccam.cmc.unn.net>
2017-03-27 19:31:18 +0300

    cpplint task 27032017

--- include/pathfinding.h
+++ include/pathfinding.h
@@ -2,32 +2,28 @@
 #define INCLUDE_PATHFINDING_H_
 
 #include <queue>
+using std::queue;
 
-queue<char> findpath(const int x, const int y)
-{
-	int cx = 0, cy = 0;
-	queue<char> way;
-	for(i = x; i < 0; i++)
-	{
-		way.push('E');
-		way.push('W');
-	}		
-	for(i = 0; i < x; i++)
-		{
-		way.push('W');
-		way.push('E');
-	}	
-	for(i = y; i < 0; i++)
-	{
-		way.push('N');
-		way.push('S');
-	}	
-	for(i = 0; i < y; i++)
-	{
-		way.push('S');
-		way.push('N');
-	}
-	return way; 
+queue<char> findpath(const int x, const int y) {
+    int cx = 0, cy = 0;
+    queue<char> way;
+    for(i = x; i < 0; i++) {
+        way.push('E');
+        way.push('W');
+    }		
+    for(i = 0; i < x; i++) {
+        way.push('W');
+        way.push('E');
+    }	
+    for(i = y; i < 0; i++) {
+        way.push('N');
+        way.push('S');
+    }	
+    for(i = 0; i < y; i++){
+        way.push('S');
+        way.push('N');
+    }
+    return way; 
 }
 
 #endif  // INCLUDE_PATHFINDING_H_
