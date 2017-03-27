8db21b81d5cca225b6be27eef4cdf73030364405
Александр <konakov@my.com>
2017-03-27 22:09:21 +0300

    Перепрошёлся под cpplint

--- include/pathfinding.h
+++ include/pathfinding.h
@@ -5,25 +5,24 @@
 using std::queue;
 
 queue<char> findpath(const int x, const int y) {
-    int cx = 0, cy = 0;
-    queue<char> way;
-    for(i = x; i < 0; i++) {
-        way.push('E');
-        way.push('W');
-    }		
-    for(i = 0; i < x; i++) {
-        way.push('W');
-        way.push('E');
-    }	
-    for(i = y; i < 0; i++) {
-        way.push('N');
-        way.push('S');
-    }	
-    for(i = 0; i < y; i++){
-        way.push('S');
-        way.push('N');
-    }
-    return way; 
+  queue<char> way;
+  for (int i = x; i < 0; i++) {
+    way.push('E');
+    way.push('W');
+  }
+  for (int i = 0; i < x; i++) {
+    way.push('W');
+    way.push('E');
+  }
+  for (int i = y; i < 0; i++) {
+    way.push('N');
+    way.push('S');
+  }
+  for (int i = 0; i < y; i++) {
+    way.push('S');
+    way.push('N');
+  }
+  return way;
 }
 
 #endif  // INCLUDE_PATHFINDING_H_
--- test/test_pathfinding.cpp
+++ test/test_pathfinding.cpp
@@ -1,33 +1,35 @@
 #include <gtest/gtest.h>
+#include <queue>
+using std::queue;
 #include "pathfinding.h"
-
-bool check_way(queue<char> & a, int x, int y) {
-    int st = 1;
-	int cx = 0, cy = 0;
-	while(!a.empty()) {
-		switch(a.front()) {
-            case 'N':
-                cx += st;
-                break;
-            case 'E':
-                cy += st;
-                break;
-            case 'W':
-                cy -= st;
-                break;
-            case 'S':
-                cx -= st;
-                break;
-            default:
-                return false;
-        }
-        st++;
+bool check_way(queue<char> a, int x, int y) {
+  int st = 1;
+  int cx = 0, cy = 0;
+  while (!a.empty()) {
+    switch (a.front()) {
+      case 'N':
+        cy += st;
+        break;
+      case 'E':
+        cx += st;
+        break;
+      case 'W':
+        cx -= st;
+        break;
+      case 'S':
+        cy -= st;
+        break;
+      default:
+        return false;
     }
-    return (cx == x) && (cy == y);
+    st++;
+    a.pop();
+  }
+  return (cx == x) && (cy == y);
 }
 
 TEST(Pathfinding, can_find_true_way) {
-    for(int x = -50; i < 50; i++)
-        for(int y = -50; i < 50; i++)
-            EXPECT_EQ(check(findpath(x, y)), true);
+  for (int x = -50; x < 50; x++)
+    for (int y = -50; y < 50; y++)
+      EXPECT_EQ(check_way(findpath(x, y), x, y), true);
 }
