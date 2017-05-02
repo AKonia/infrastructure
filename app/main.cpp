816c38b36a7049878c814d46d844f2d587494404
Александр <konakov@my.com>
2017-05-03 00:17:20 +0300

    Выполненная лабораторная по авл-деревьям, кучам и поиску на графах

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,36 @@
-#include "add.h"
 #include <iostream>
+#include <fstream>
 
-int main() {
-  std::cout << "2 + 2 = " << add(2, 2) << std::endl;
+#include "pathfinding_dijkstra.h"
+
+using namespace std;
+
+int main()
+{
+    int n, from, to, weight;
+    n = from = to = weight = 0;
+    ifstream in("graph.in");
+       
+    in >> n;
+    Graph<bool> g(n);
+    while(!in.eof())
+    {
+        in >> from >> to >> weight;
+        g.setEdge(from, to, weight);
+    }
+    in.close();
+
+    cout << "<<< Pathfinding based on PriorityQueue >>>" << endl;    
+    list < Vertex<int> > waypoints = findShortestPathPQ(g, 0, 6);
+    for(list< Vertex<int> >::iterator i = waypoints.begin(); i != waypoints.end(); ++i)
+        cout << "WayPoint: " << i->getData() << endl;
+    cout << endl;
+
+    cout << "<<< Pathfinding based on AVLTrees >>>" << endl;    
+    waypoints = findShortestPathTree(g, 0, 6);
+    for(list< Vertex<int> >::iterator i = waypoints.begin(); i != waypoints.end(); ++i)
+        cout << "WayPoint: " << i->getData() << endl;
+    cout << endl;
+
+    return 0;
 }
