9edc277333cf7afb665796527dbb966372f76582
Александр <konakov@my.com>
2017-05-13 21:40:04 +0300

    Выполненное ТК по разделённым множествам

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,37 @@
-#include "add.h"
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
 
-int main() {
-  std::cout << "2 + 2 = " << add(2, 2) << std::endl;
+#include "percolation.h"
+
+using namespace std;
+
+int main()
+{
+    const uint fieldSize = 100;
+    srand(time(0));
+
+    uint field[fieldSize];
+    memset(field, 0, sizeof(field));
+
+    vector<int> v(fieldSize);
+    for(uint32_t i = 0; i < v.size(); i++)
+        v[i] = i;
+
+    int count = 0;
+    for(uint32_t i = 0; i < 1000; ++i)
+    {
+        for(uint32_t k = 0;!percolationTest(field, 10); ++k)
+        {
+            field[v[k]] = 1;
+            ++count;
+        }
+        random_shuffle(v.begin(), v.end());
+        memset(field, 0, sizeof(field));
+    }
+    cout << endl << "Vacancy probability: " << (count / 1000.0f) << "%" << endl;
+    return 0;
 }
---
+++ include/percolation.h
@@ -0,0 +1,11 @@
+// Copyright 2017 AKonia
+#ifndef INCLUDE_PERCOLATION_H_
+#define INCLUDE_PERCOLATION_H_
+
+#include <cstdint>
+
+#include "unionfind.h"
+
+bool percolationTest(uint32_t *field, uint32_t width);
+
+#endif  // INCLUDE_PERCOLATION_H_
