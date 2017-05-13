519eb9ad0aad028cafff070ad3cabd167908b964
Александр <konakov@my.com>
2017-05-13 23:35:47 +0300

    Пролинтовал приложение

--- app/main.cpp
+++ app/main.cpp
@@ -1,37 +1,35 @@
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
+// Copyright 2017 AKonia
 #include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <vector>
 
 #include "percolation.h"
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::vector;
 
-int main()
-{
-    const uint fieldSize = 100;
-    srand(time(0));
+int main() {
+  srand(time(0));
 
-    uint field[fieldSize];
-    memset(field, 0, sizeof(field));
+  uint32_t field[100];
+  memset(field, 0, sizeof(field));
 
-    vector<int> v(fieldSize);
-    for(uint32_t i = 0; i < v.size(); i++)
-        v[i] = i;
+  vector<int> v(100);
+  for (uint32_t i = 0; i < v.size(); i++) v[i] = i;
 
-    int count = 0;
-    for(uint32_t i = 0; i < 1000; ++i)
-    {
-        for(uint32_t k = 0;!percolationTest(field, 10); ++k)
-        {
-            field[v[k]] = 1;
-            ++count;
-        }
-        random_shuffle(v.begin(), v.end());
-        memset(field, 0, sizeof(field));
+  int count = 0;
+  for (uint32_t i = 0; i < 1000; ++i) {
+    for (uint32_t k = 0; !percolationTest(field, 10); ++k) {
+      field[v[k]] = 1;
+      ++count;
     }
-    cout << endl << "Vacancy probability: " << (count / 1000.0f) << "%" << endl;
-    return 0;
+    random_shuffle(v.begin(), v.end());
+    memset(field, 0, sizeof(field));
+  }
+  cout << endl << "Vacancy probability: " << (count / 1000.0f) << "%" << endl;
+  return 0;
 }
